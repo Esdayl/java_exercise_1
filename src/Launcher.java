159fccc57594295edc740d0a5b230fdf6fae6504
Esdayl <romain.chevallier@epita.fr>
2021-05-26 10:52:43 +0200

    refactoring

--- src/Launcher.java
+++ src/Launcher.java
@@ -14,65 +14,90 @@ public class Launcher {
         System.out.println("Welcome !");
         Scanner scan = new Scanner(System.in);
         String entry;
-        while (!(entry = scan.nextLine()).equals("quit")) {
-            switch(entry)
-            {
-                case "fibo":
-                {
-                    System.out.println("Enter a positive number :");
-                    long n = Long.parseLong(scan.nextLine());
-                    // Handle negative number
-                    if (n < 0) {
-                        System.err.println("This number is not positive...");
-                        continue;
-                    }
-                    System.out.println(fibo(n));
-                }
-                case "freq":
-                {
-                    System.out.println("Enter a path");
-                    try {
-                        Path path = Paths.get(scan.nextLine());
-                        String text = Files.readString(path);
-                        String[] words = text.split(" ");
-
-                        Map<String, Long> countsByWord = Arrays.stream(words)
-                                .filter(s -> !s.isBlank())
-                                .map(s->s.toLowerCase())
-                                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-
-                        Function<Map.Entry<String, Long>, Long> extractor = e->e.getValue();
-                        String w = countsByWord.entrySet().stream()
-                                .sorted(Comparator.comparing(extractor).reversed())
-                                .limit(3).map(e->e.getKey())
-                                .collect(Collectors.joining(" "));
-                        System.out.println(w);
-                    }
-                    catch (InvalidPathException e) {
-                        System.err.println("This path is not valid...");
-                    }
-                    catch (IOException e) {
-                        System.err.println("Unreadable file: " + e.getClass() + e.getMessage());
-                    }
-                }
-                default:
-                    System.out.println("Unknown command");
-            }
+        boolean ok = true;
+        List<Command> cmds = List.of(new Quit(), new Fibo(), new Freq());
+        while (ok) {
+            System.out.println("Enter a command :");
+            entry = scan.nextLine();
+            for (Command cmd : cmds)
+                if (entry.equals(cmd.name()))
+                    ok = cmd.run(scan);
         }
     }
-    static long fibo(long n)
-    {
-        if (n < 2)
-            return n;
-        long u0 = 0;
-        long u1 = 1;
-        long tmp;
-        for (long i = 1; i < n; i++)
-        {
-            tmp = u0;
-            u0 = u1;
-            u1 += tmp;
+
+    public interface Command {
+        String name();
+        boolean run(Scanner scan);
+    }
+
+    public static class Quit implements Command {
+        @Override
+        public String name() {
+            return "quit";
         }
-        return u1;
+        @Override
+        public boolean run(Scanner scan) {
+            System.out.println("Goodbye !");
+            return false;
+        }
+    }
+
+    public static class Fibo implements Command {
+        @Override
+        public String name() {
+            return "fibo";
+        }
+        @Override
+        public boolean run(Scanner scan) throws ArithmeticException {
+            long n = Long.parseLong(scan.nextLine());
+            // Handle negative number
+            if (n < 0) {
+                System.err.println("This number is not positive...");
+                return false;
+            }
+            System.out.println(fibo(n));
+            return true;
+        }
+    }
+
+    public static class Freq implements Command {
+        @Override
+        public String name() {
+            return "freq";
+        }
+        @Override
+        public boolean run(Scanner scan) {
+            try {
+                System.out.println("Enter the file path :");
+                Path path = Paths.get(scan.nextLine());
+                String text = Files.readString(path);
+                String[] words = text.split(" ");
+
+                Map<String, Long> countsByWord = Arrays.stream(words)
+                        .filter(s -> !s.isBlank())
+                        .map(s->s.toLowerCase())
+                        .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+
+                Function<Map.Entry<String, Long>, Long> extractor = e->e.getValue();
+                String w = countsByWord.entrySet().stream()
+                        .sorted(Comparator.comparing(extractor).reversed())
+                        .limit(3).map(e->e.getKey())
+                        .collect(Collectors.joining(" "));
+                System.out.println(w);
+                return true;
+            }
+            catch (InvalidPathException e) {
+                System.err.println("This path is not valid...");
+            }
+            catch (IOException e) {
+                System.err.println("Unreadable file: " + e.getClass() + e.getMessage());
+            }
+            return false;
+        }
+    }
+
+    private static long fibo(long n)
+    {
+        return n == 0 || n == 1 ? n : fibo(n + 1) + fibo(n + 2);
     }
 }
